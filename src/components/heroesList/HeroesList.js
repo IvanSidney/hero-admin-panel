f9ac69257852ecc43725a6d3a11f20d3bd3f3ef3
IvanSidney <ivansidney7@gmail.com>
2023-02-13 12:15:28 +0200

    new 13/02 Redux toolkit createEntityAdapter()

--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -2,27 +2,14 @@ import {useHttp} from '../../hooks/http.hook';
 import { useCallback, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {CSSTransition, TransitionGroup} from 'react-transition-group';
-import { createSelector } from '@reduxjs/toolkit'; 
 
-import { heroDeleted, fetchHeroes } from '../heroesList/heroesSlice';
+import { heroDeleted, fetchHeroes, filteredHeroesSelector } from '../heroesList/heroesSlice';
 import HeroesListItem from "../heroesListItem/HeroesListItem";
 import Spinner from '../spinner/Spinner';
 
 import './heroesList.scss';
 
 const HeroesList = () => {
-
-    const filteredHeroesSelector = createSelector(
-        (state) => state.filters.activeFilter,
-        (state) => state.heroes.heroes,
-        (filter, heroes) => {
-            if (filter === 'all') {
-                return heroes;
-            } else {
-                return heroes.filter(item => item.element === filter);
-            }
-        }
-    );
     
     const filteredHeroes = useSelector(filteredHeroesSelector);
     const heroesLoadingStatus = useSelector(state => state.heroes.heroesLoadingStatus);
@@ -40,7 +27,7 @@ const HeroesList = () => {
         .then(dispatch(heroDeleted(id)))
         .catch(err => console.log(err))
     // eslint-disable-next-line
-}, [request]);
+    }, [request]);
 
     if (heroesLoadingStatus === "loading") {
         return <Spinner/>;
